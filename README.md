3473dcb90d22956ffb1f037179978023a6f2bc29
PalermoD <palermo.deschamps@gmail.com>
2015-06-10 12:56:05 -0400

    added instuctions to the readme

---
+++ README.md
@@ -0,0 +1,7 @@
+## Bluewolf coding challenge
+
+Built on ruby and uses rspec for tests 
+
+gem install rspec 
+
+run `rspec` in terminal in bluewolf directory to run tests
--- spec/challenge_spec.rb
+++ spec/challenge_spec.rb
@@ -1,14 +1,14 @@
 require 'challenge'
 require 'rspec'
 
-describe 'find_duplicate' do
+describe 'find duplicate number in array' do
   it "find the integer in the array that is a duplicate" do
     expect(find_duplicate(
       [1,2,3,4,25,200,400,600,100000,25]
     )).to eq(25)
   end
 
-  it "returns nil if there is no duplicate" do
+  it "returns nil if there is no duplicates in array" do
     expect(find_duplicate(
       [1,2,3,4,25,200,400,600,100000,2500]
     )).to eq(nil)
